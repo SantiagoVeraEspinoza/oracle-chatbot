d491744c242b375087c1c283f012c26da8ebb083
Samuel <a01641383@tec.mx>
2024-05-28 13:57:42 -0600

    Cambio mensje faltante de traducir

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
@@ -8,7 +8,7 @@ public enum BotMessages {
 	ITEM_DONE("¡Tarea completada! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."), 
 	ITEM_UNDONE("¡Tarea activada! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."), 
 	ITEM_DELETED("¡Tarea eliminada! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."),
-	TYPE_NEW_TODO_ITEM("Type a new todo item below and press the send button (blue arrow) on the rigth-hand side."),
+	TYPE_NEW_TODO_ITEM("Escribe el titulo de una nueva tarea y presiona el botón de enviar (flecha azul) en el lado derecho."),
 	NEW_ITEM_ADDED("Escribe un nuevo elemento de la lista de tareas a continuación y presiona el botón de enviar (flecha azul) en el lado derecho."),
 	BYE("¡Adiós! Selecciona /start para continuar.");
 
